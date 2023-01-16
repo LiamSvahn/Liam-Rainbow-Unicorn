f37bd325c4720bfcfc6406dc52ec9d791dab4577
liam <01liasyr@skola.boras.se>
2023-01-16 12:02:32 +0100

    la till sista övningen och koden suns i konsolen

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
-var arr = ["apa", 'ourangutang', 'gorila', 'get', 'giraff'];
-
-
-console.log(arr);
+var arr01 = ["apa", 'ourangutang', 'gorila', 'get', 'giraff'];
+var arr1 = ['hej', true, 2];
+function foo(s) {
+    console.log(s + ' hejsan på dig');
+}
+foo("hej");
+console.log(foo);
+console.log(arr01);
+console.log(arr1);
--- app.ts
+++ app.ts
@@ -1,3 +1,12 @@
-const arr:string[] = ["apa", 'ourangutang', 'gorila', 'get', 'giraff']
+const arr01:(string)[] = ["apa", 'ourangutang', 'gorila', 'get', 'giraff']
 
-console.log(arr)
+const arr1:(string | boolean | number)[] = ['hej', true, 2]
+
+function foo(s: string){
+    console.log(s + ' hejsan på dig')
+}
+foo("hej")
+
+console.log(foo)
+console.log(arr01)
+console.log(arr1)
